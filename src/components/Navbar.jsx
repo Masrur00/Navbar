d96b1d8f1e7421b5526f9e74526afa6ad56a188f
Masrur00 <masrur.jsr@gmail.com>
2022-05-16 19:12:00 +0530

    3 files modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 
+import logo from '../logo.svg'
 
 export const Navbar = ()=> {
    const links = ["Services", "Projects", "About"];
    return <>
-   
+   <img className="App-logo" src={logo} alt="" />
+   {
+     links.map(res => 
+      <span>{res}</span>
+      )
+   }
+   <button className="btn-contact" >Contact</button>
    </>
 }
